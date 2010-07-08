fada0a949e1a9bf5ad90872a1d0729b76dc05cc4
Jacques Crocker <railsjedi@gmail.com>
2010-07-07 23:53:50 -0700

    Only require ActiveSupport core_extensions

---
+++ lib/jammit/dependencies.rb
@@ -0,0 +1,30 @@
+# Standard Library Dependencies:
+require 'uri'
+require 'erb'
+require 'zlib'
+require 'base64'
+require 'pathname'
+require 'fileutils'
+
+require 'yaml'
+
+# Gem Dependencies:
+require 'yui/compressor'
+require 'closure-compiler'
+require 'active_support'
+
+# Load initial configuration before the rest of Jammit.
+Jammit.load_configuration(Jammit::DEFAULT_CONFIG_PATH) if defined?(Rails)
+
+# Jammit Core:
+require 'jammit/compressor'
+require 'jammit/packager'
+
+# Jammit Rails Integration:
+if defined?(Rails)
+  require 'jammit/controller'
+  require 'jammit/helper'
+  require 'jammit/railtie'
+  require 'jammit/routes'
+end
+
